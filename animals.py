dd42ef7f7ba1b3b839ac50512c1a872461303ab8
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-10-13 13:39:07 -0500

    all animals in classes

---
+++ animals.py
@@ -0,0 +1,119 @@
+class Llama():
+    def __init__(self, unique_id, name, species):
+        self.id = unique_id
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Goat():
+    def __init__(self, unique_id, name, species):
+        self.id = unique_id
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Horse():
+    def __init__(self, unique_id, name, species):
+        self.id = unique_id
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Pig():
+    def __init__(self, unique_id, name, species):
+        self.id = unique_id
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Cow():
+    def __init__(self, unique_id, name, species):
+        self.id = unique_id
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class StingRay():
+    def __init__(self, unique_id, name, species):
+        self.id = unique_id
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+class Goldfish():
+    def __init__(self, unique_id, name, species):
+        self.id = unique_id
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+class Catfish():
+    def __init__(self, unique_id, name, species):
+        self.id = unique_id
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+class Otter():
+    def __init__(self, unique_id, name, species):
+        self.id = unique_id
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+class Sharks():
+    def __init__(self, unique_id, name, species):
+        self.id = unique_id
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+class Python():
+    def __init__(self, unique_id, name, species):
+        self.id = unique_id
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+class Crocodile():
+    def __init__(self, unique_id, name, species):
+        self.id = unique_id
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+class Salamander():
+    def __init__(self, unique_id, name, species):
+        self.id = unique_id
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+class Anaconda():
+    def __init__(self, unique_id, name, species):
+        self.id = unique_id
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+class Gecko():
+    def __init__(self, unique_id, name, species):
+        self.id = unique_id
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
